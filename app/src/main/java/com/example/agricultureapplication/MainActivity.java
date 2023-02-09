195e32ce4c054a645dbc3d1644d2cbb8b4d49ac8
Ali <ozdemirali@hotmail.com>
2023-02-09 23:24:21 +0300

    fetch data from Webapi

--- app/src/main/java/com/example/agricultureapplication/MainActivity.java
+++ app/src/main/java/com/example/agricultureapplication/MainActivity.java
@@ -13,17 +13,49 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.view.Menu;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.net.URLConnection;
+import java.util.concurrent.atomic.AtomicReference;
+
+import security.HttpsTrustManager;
+
+public class MainActivity<requestQueue> extends AppCompatActivity {
 
     private ImageView imageView;
+    private Button buttonSave;
     public static final int RequestPermissionCode = 1;
     ActivityResultLauncher<Intent> activityResultLauncher;
+    private RequestQueue mRequestQueue;
+    private StringRequest mStringRequest;
+    private String url = "https://192.168.1.245:45455/api/Type";
+    private static final String TAG = MainActivity.class.getName();
+
+
 
 
     @Override
@@ -31,7 +63,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageView=findViewById(R.id.imageView);
+        buttonSave=findViewById(R.id.buttonSave);
 
+        //Instantiate the RequestQueue.
+        RequestQueue queue= Volley.newRequestQueue(this);
+        //RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String _url="https://192.168.1.245:45455/api/Type";
+
+        //String url="https://jsonplaceholder.typicode.com/todos/";
 
         //Give camera permission
         EnableRuntimePermission();
@@ -76,6 +115,37 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Tıklandı");
+
+                HttpsTrustManager.allowAllSSL();
+                StringRequest myRequest = new StringRequest(Request.Method.GET, url,
+                        response -> {
+                            try{
+                                System.out.println(response.toString());
+                                JSONArray jsonarray = new JSONArray(response);
+
+                                for (int i=0; i < jsonarray.length(); i++) {
+                                    System.out.println(jsonarray.getJSONObject(i));
+                                }
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        },
+                        volleyError -> Toast.makeText(MainActivity.this, volleyError.getMessage(), Toast.LENGTH_SHORT).show()
+                );
+                //RequestQueue requestQueue = Volley.newRequestQueue(this);
+                queue.add(myRequest);
+                //sendAndRequestResponse();
+
+
+
+            }
+        });
+
 
     }
 
@@ -103,6 +173,64 @@ public class MainActivity extends AppCompatActivity {
                 }
                 break;
         }
+    }
+
+
+    private void sendAndRequestResponse() {
+
+        //RequestQueue initialized
+        mRequestQueue = Volley.newRequestQueue(this);
+
+        //String Request initialized
+        mStringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                Toast.makeText(getApplicationContext(),"Response :" + response.toString(), Toast.LENGTH_LONG).show();//display the response on screen
+                System.out.println(response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                System.out.println("Error :" + error.getMessage());
+                Log.i(TAG,"Error :" + error.toString());
+            }
+        });
+
+        mRequestQueue.add(mStringRequest);
+    }
+
+    private void test(){
+        // Request a string response from the provided URL.
+        mRequestQueue = Volley.newRequestQueue(this);
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String status = response.getString("status");
+                            String message = response.getString("message");
+                            Toast.makeText(getApplicationContext(), ""+status+message, Toast.LENGTH_SHORT).show();
+
+
+                        } catch (JSONException e) {
+                            //e.printStackTrace();
+                            System.out.println("Catch Error");
+                            System.out.println(e.getMessage());
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //error.printStackTrace();
+                        System.out.println("Error Listener");
+                        //System.out.println(error.getMessage());
+                    }
+                });
+        mRequestQueue.add(jsonObjectRequest);
+
+         //Add the request to the RequestQueue.
+        //queue.add(stringRequest);
     }
 
 
